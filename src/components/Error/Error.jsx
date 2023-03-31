f54b57d7e97cf7416941f3c8bfeaea65507c3543
mejrimo <mejrimo.dev@gmail.com>
2023-03-31 21:37:03 +0200

    error message

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,6 +1,6 @@
 import './error.css';
 
 const Error = () => {
-	return <h2 className="error-message">There was an error...</h2>;
+	return <h2 className="error-message">Sorry! There was an error...</h2>;
 };
 export default Error;
