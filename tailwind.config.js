c51fdc45ee517011552700a4e8ba2384d56be442
mejrimo <mejrimo.dev@gmail.com>
2023-04-04 02:56:46 +0200

    config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+		colors: {
+			crema: '#F0E6E0',
+			verde: '#607D59',
+			pisello: '#7F8F67',
+			grigio: '#9ca3af',
+			grigioscuro: '#4b5563',
+			bianco: '#ffffff',
+		},
+	},
+	plugins: [],
+};
