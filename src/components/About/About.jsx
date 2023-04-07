487e0d28ea4ca5bc4bad52200e2e28f7612ff4ba
mejrimo <mejrimo.dev@gmail.com>
2023-04-07 05:07:09 +0200

    style fixed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,16 +4,16 @@ import { useNavigate } from 'react-router-dom';
 const About = () => {
 	const navigate = useNavigate();
 	return (
-		<div className="flex items-center justify-center">
-			<div className="mt-5 mx-5 lg:mt-10 lg:mx-10 flex flex-col">
+		<div className="flex items-center justify-center h-screen">
+			<div className="mx-5 lg:mx-10 flex flex-col">
 				<div className="mb-5 flex items-center justify-between">
 					<AiOutlineArrowLeft
 						className="text-2xl md:text-3xl lg:text-4xl cursor-pointer active:text-pisello lg:hover:text-pisello transition-all duration-200"
 						onClick={() => navigate(-1)}
 					/>
-					<h2 className="mx-auto text-2xl md:text-4xl lg:text-5xl">About</h2>
+					<h2 className="mx-auto text-2xl md:text-3xl lg:text-4xl">About</h2>
 				</div>
-				<p className="text-sm md:text-xl lg:text-2xl text-center">
+				<p className="text-grigioscuro text-base lg:text-lg xl:text-xl break-words text-center md:text-left">
 					Welcome to this project! This website was created with React, a popular JavaScript library
 					for building dynamic user interfaces. Specifically, I used React version 18.2.0 to handle
 					user interaction with the page and render changes in real-time. Additionally, I utilized
