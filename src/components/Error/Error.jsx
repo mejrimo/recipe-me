83c4bec3af2b6d402999a712fc0d0d58422ba3aa
mejrimo <mejrimo.dev@gmail.com>
2023-04-07 04:49:05 +0200

    font size adjusted

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,6 +1,6 @@
 const Error = () => {
 	return (
-		<h2 className="flex items-center justify-center h-screen text-2xl text-center md:text-4xl lg:text-5xl">
+		<h2 className="flex items-center justify-center h-screen text-2xl text-center md:text-3xl lg:text-4xl">
 			Sorry ! There was an error, try with another recipe...
 		</h2>
 	);
