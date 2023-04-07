43274552dd48a1b6a36a63eab333cacee6d14681
mejrimo <mejrimo.dev@gmail.com>
2023-04-07 04:51:41 +0200

    Link to /recipe/:id added

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import striptags from 'striptags';
 
-const Recipe = ({ title, image, url, summary }) => {
+const Recipe = ({ id, title, image, summary }) => {
 	return (
 		<div className="mb-5 w-72 md:w-80 lg:w-96 justify-self-center cursor-pointer shadow-md rounded-lg transform hover:shadow-lg  hover:-translate-y-1 transition-all duration-200">
-			<a href={url} target="_blank" rel="noopener noreferrer">
+			<Link to={`/recipe/${id}`}>
 				<img className="rounded-lg w-full" src={image} alt={`${title} image`} />
 				<h2 className="text-2xl m-2 hover:translate-x-1 transition-all duration-200">{title}</h2>
-			</a>
+			</Link>
 			<p className="m-2 text-sm text-grigioscuro line-clamp-4">{striptags(summary)}</p>
 		</div>
 	);
