46125d5353ac71399f9065ae448d4a89393cad54
mejrimo <mejrimo.dev@gmail.com>
2023-04-14 01:29:57 +0200

    useContext changed

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,15 +1,37 @@
+import { useContext } from 'react';
+import { AppContext } from '../../App';
 import { Link } from 'react-router-dom';
 import striptags from 'striptags';
 
 const Recipe = ({ id, title, image, summary }) => {
-	return (
-		<div className="mb-5 w-72 md:w-80 lg:w-96 justify-self-center cursor-pointer shadow-md rounded-lg transform hover:shadow-lg  hover:-translate-y-1 transition-all duration-200">
-			<Link to={`/recipe/${id}`}>
-				<img className="rounded-lg w-full" src={image} alt={`${title} image`} />
-				<h2 className="text-2xl m-2 hover:translate-x-1 transition-all duration-200">{title}</h2>
-			</Link>
-			<p className="m-2 text-sm text-grigioscuro line-clamp-4">{striptags(summary)}</p>
-		</div>
-	);
+	const { recipes } = useContext(AppContext);
+
+	const recipesElements = recipes?.map((recipe) => {
+		const { id, title, image, summary } = recipe;
+
+		return (
+			<div
+				key={id}
+				className="mb-5 w-72 md:w-80 lg:w-96 justify-self-center cursor-pointer shadow-md rounded-lg transform hover:shadow-lg  hover:-translate-y-1 transition-all duration-200">
+				<Link to={`/recipe/${id}`}>
+					<img className="rounded-lg w-full" src={image} alt={`${title} image`} />
+					<h2 className="text-2xl m-2 hover:translate-x-1 transition-all duration-200">{title}</h2>
+				</Link>
+				<p className="m-2 text-sm text-grigioscuro line-clamp-4">{striptags(summary)}</p>
+			</div>
+		);
+	});
+
+	return <>{recipesElements}</>;
 };
 export default Recipe;
+
+// return (
+// 	<div className="mb-5 w-72 md:w-80 lg:w-96 justify-self-center cursor-pointer shadow-md rounded-lg transform hover:shadow-lg  hover:-translate-y-1 transition-all duration-200">
+// 		<Link to={`/recipe/${id}`}>
+// 			<img className="rounded-lg w-full" src={image} alt={`${title} image`} />
+// 			<h2 className="text-2xl m-2 hover:translate-x-1 transition-all duration-200">{title}</h2>
+// 		</Link>
+// 		<p className="m-2 text-sm text-grigioscuro line-clamp-4">{striptags(summary)}</p>
+// 	</div>
+// )
--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -1,20 +1,10 @@
-import { useContext } from 'react';
-import { AppContext } from '../../App';
 import Recipe from '../Recipe/Recipe';
 
 const Recipes = () => {
-	const { recipes } = useContext(AppContext);
-
-	const recipesElements = recipes.map((recipe) => {
-		const { id, title, image, summary } = recipe;
-
-		return <Recipe key={id} id={id} title={title} image={image} summary={summary} />;
-	});
-
 	return (
 		<div className="container mx-auto ">
 			<div className="grid my-10 mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-5">
-				{recipesElements}
+				<Recipe />
 			</div>
 		</div>
 	);
